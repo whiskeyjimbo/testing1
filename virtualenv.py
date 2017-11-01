ddaa2e7c987e73b5978fc7885aa6a63690ef27be
jeff rose <mrjeffrose@gmail.com>
2017-10-31 17:42:26 -0700

    fixing

--- virtualenv.py
+++ virtualenv.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 from subprocess import call
 import sys
-#os.system("sudo apt-get install -y python-virtualenv")
+os.system("sudo apt-get install -y python-virtualenv")
 call(["virtalenv", str(sys.argv[1:])])
